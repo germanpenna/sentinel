011500aba536cff5dda3264d624113db83c332be
German Pena <121510096+germanpenna@users.noreply.github.com>
2026-02-23 17:52:06 -0500

    Revert "Make Stripe webhook public (exclude from Clerk middleware)"
    This reverts commit 65e3cd24564a5880a21d9ad2b5ead2960c408a80.

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(["/app(.*)"]);
-const isPublicApiRoute = createRouteMatcher(["/api/stripe(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isPublicApiRoute(req)) {
-    return NextResponse.next();
-  }
   if (isProtectedRoute(req)) {
     const { userId } = await auth();
     if (!userId) {
