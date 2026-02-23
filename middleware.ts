2b8f921014a2a235d0b51906b1007831224d5f88
German Pena <121510096+germanpenna@users.noreply.github.com>
2026-02-23 17:52:05 -0500

    Revert "Fixing issues with Stripe 401"
    This reverts commit 149aa8ed7298a31a6ae3063635eea83bacc6a1fa.

--- middleware.ts
+++ middleware.ts
@@ -19,7 +19,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    "/((?!_next|api/stripe/webhook|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
   ],
 };
